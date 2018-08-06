839ab47a171664ef6ee72bbd50cb84db421df673
Ndife <stilesndife@gmail.com>
2018-08-06 17:00:32 +0100

    added signup method to controller

--- Backend/controllers/users.js
+++ Backend/controllers/users.js
@@ -1,14 +1,54 @@
+const User = require('../models/users');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
-const userScheme = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    email: {type: String, 
-        required: true, 
-        unique: true,
-        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
-    },
-    password: {type:String, required:true},
-    verified: {type:Boolean, require:true}
-})
 
-module.exports = mongoose.model('User', userScheme);
+
+exports.signUp = (req,res,next)=>{
+    User.find({email: req.body.email})
+    .exec()
+    .then(user =>{
+        if(user.length>=1){
+            return res.status(409).json({
+                    message: 'email already exist'
+                }   
+            )
+        }else {
+            bcrypt.hash(req.body.password,10, (err, hash)=>{
+                if(err){
+                   return res.status(500).json({err:err});
+                }else {
+                    const user = new User({
+                        _id: new mongoose.Types.ObjectId(), 
+                        email: req.body.email,
+                        password: hash, 
+                        verified:false
+                    })
+                    user.save()
+                    .then(docs =>{
+                        return res.status(201).json({
+                            message: 'User created successfully',
+                            email: docs.email,
+                            verified: docs.verified,
+                            request: {
+                                type: 'GET',
+                                url: 'http://localhost:500/users/' + docs.email
+                            }
+                        })
+                     
+                    })
+                    .catch(err =>{
+                        res.status(500).json({
+                            ErrMessage: 'Sorry error occur',
+                            err: err
+                        });
+                    })
+                
+                }
+    
+            
+            })
+        }
+    })
+       
+}
