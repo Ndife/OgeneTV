045aa88f7a679a863171c90a2a86133b21ee2c1e
Ndife <stilesndife@gmail.com>
2018-08-06 17:11:38 +0100

    added route for email verification

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const users = require('../controllers/users');
 
 router.post('/signUp', users.signUp);
-
+router.post('/verify/:email', users.verify);
 
 module.exports = router;
