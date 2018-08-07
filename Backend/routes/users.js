009e4f7a42885ef1531c40a707a9a2e27bf084ef
Ndife <stilesndife@gmail.com>
2018-08-07 10:21:39 +0100

    added users login route

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -4,5 +4,6 @@ const users = require('../controllers/users');
 
 router.post('/signUp', users.signUp);
 router.post('/verify/:email', users.verify);
+router.post('/login', users.logIn);
 
 module.exports = router;
