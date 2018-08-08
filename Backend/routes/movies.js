568515432b68d60851c29e29971c6bb0946885aa
IfeomaLucia <nwosuifeomalucia@gmail.com>
2018-08-08 21:44:14 +0100

    Changed DELETE request to GET

--- Backend/routes/movies.js
+++ Backend/routes/movies.js
@@ -12,7 +12,7 @@ router.post('/', upload.single('movieFile'), movieController.addMovie);
 //GET all the movies listing
 router.get('/', movieController.getAllMovies);
 //DELETE a movie by passing its Id
-router.delete('/:id', movieController.deleteMovie);
+router.get('/delete/:id', movieController.deleteMovie);
 //POST an update to a movie by passing its Id
 router.post('/:id', movieController.updateMovie);
 //GET a particular movie by any param eg name, producer
