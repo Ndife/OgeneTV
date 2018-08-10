48b7571af4ca450b1f0409c01b4ff6f7868f7e63
buka4chocksy <buka4chocksy@gmail.com>
2018-08-10 14:14:27 -0700

    adjusted the home route

--- Backend/routes/home.js
+++ Backend/routes/home.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req,res)=>{
-    res.render('index');
+    res.render('hello world');
 })
 module.exports = router;
