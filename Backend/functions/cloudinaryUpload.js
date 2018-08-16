319d6e7d4205b31f71e3b82ff060ebd7d755851e
IfeomaLucia <nwosuifeomalucia@gmail.com>
2018-08-16 14:29:36 +0100

    added the cloudinary package

---
+++ Backend/functions/cloudinaryUpload.js
@@ -0,0 +1,23 @@
+var cloudinary = require('cloudinary');
+
+cloudinary.config({
+    cloud_name: 'ogenetv', 
+    api_key: '379334756145365', 
+    api_secret: 'KDNbVDKcu7VctfxkDgZiWX04My8' 
+})
+
+eexports.upload = function(file){
+    return new Promise(resolve => {
+        cloudinary.uploader.upload(file, function(result){
+            resolve({url: result.url, Id: result.public_id});
+        })
+   })
+}
+
+exports.delete = function(publicId){
+    return new Promise(resolve => {
+        cloudinary.uploader.destroy(publicId, function(result){
+            resolve(result);
+        })
+    })
+}
