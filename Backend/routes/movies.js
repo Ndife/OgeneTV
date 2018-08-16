cc18847f92e295ba8c1f589ffc3000846c5b7ef6
IfeomaLucia <nwosuifeomalucia@gmail.com>
2018-08-16 11:59:49 +0100

    added the search movie function

--- Backend/routes/movies.js
+++ Backend/routes/movies.js
@@ -8,7 +8,7 @@ var movieController = require('../controllers/movies');
 var upload = require('../movieUpload')
 
 
-router.post('/', upload.any('movieFile'), movieController.addMovie);
+router.post('/', upload.any(), movieController.addMovie);
 //GET all the movies listing
 router.get('/', movieController.getAllMovies);
 //DELETE a movie by passing its Id
@@ -16,7 +16,9 @@ router.get('/delete/:id', movieController.deleteMovie);
 //POST an update to a movie by passing its Id
 router.post('/:id', movieController.updateMovie);
 //GET a particular movie by any param eg name, producer
-router.get('/search', movieController.getByParam);
+router.get('/get', movieController.getByParam);
+
+router.get('/search/:value', movieController.searchMovie);
 //GET a movie by its Id
 router.get('/:id', movieController.getById);
 
