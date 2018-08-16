84f4fc01d8321b7f2d03aade330247a7536ff4cd
Ndife <stilesndife@gmail.com>
2018-08-16 13:40:09 +0100

    added a route for user authentication

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -2,9 +2,10 @@
 const express = require('express');
 const router = express.Router();
 const users = require('../controllers/users');
+const checkAuth = require('../functions/check-auth');
 
-router.post('/signUp', users.signUp);
-router.get('/verify/:email', users.verify);
+router.post('/signUp',users.signUp);
+router.get('/verify/:email',users.verify);
 router.post('/login', users.logIn);
 
 module.exports = router;
