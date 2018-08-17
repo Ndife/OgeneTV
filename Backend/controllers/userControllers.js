45a5456c7c6f179dc997ccd1a6fe6361eb788594
buka4chocksy <buka4chocksy@gmail.com>
2018-08-17 01:37:18 -0700

    required movies model and mongoose objectID. also added getmovies function which helps the user to add movies to his movies folder

--- Backend/controllers/userControllers.js
+++ Backend/controllers/userControllers.js
@@ -3,6 +3,10 @@ var facebookStartegy = require('passport-facebook').Strategy;
 var key = require('../config/key');
 var User = require('../models/user1');
 var bcrypt = require('bcrypt');
+var movieModel = require('../models/movies');
+const ObjectID = require('mongoose').Types.ObjectId;
+
+
 //passport sign up with facebook
 
 passport.serializeUser((user,done)=>{
@@ -61,6 +65,42 @@ passport.use(new facebookStartegy({
   
     
  }))
+
+
 module.exports = {
     passport: passport
 };
+
+
+exports.getMovies = function(req, res){
+    var user = new ObjectID(req.body.user)
+    var movie = new ObjectID (req.body.movie)
+    User.findById({_id:user}, function(err, data){
+        if(data){
+            movieModel.findOne({_id:movie} , function(err, data2){
+                if(!data2){
+                    res.json({err:err, message:'Movies Not Found !!'})
+
+                }else{
+             
+                    
+                    if(JSON.stringify(data.movies).includes(JSON.stringify(data2._id))  ){
+                        res.json({err:err, message:'Movie Already Exists In Library!!'})
+
+                    }else{
+                    data.movies.push(data2._id)
+                    data.save()
+                    res.json({message:'Movie Purchase Successful'})
+                    }
+
+                }
+
+             
+            })
+        }else{
+            res.json({err:err,message:'User Not Found'})
+        }
+    })
+
+}
+
