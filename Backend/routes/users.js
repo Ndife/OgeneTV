33337cb15c49f8a7dc49d306f9e90fe973d28d53
buka4chocksy <buka4chocksy@gmail.com>
2018-08-17 01:42:29 -0700

    added userAddmovie to user routes and user view movies in ther user routes

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -1,11 +1,14 @@
 
-const express = require('express');
-const router = express.Router();
-const users = require('../controllers/users');
+var express = require('express');
+var router = express.Router();
+var users = require('../controllers/users');
+var user1 = require('../controllers/userControllers')
 
 router.post('/signUp', users.signUp);
 router.post('/verify/:email', users.verify);
 router.post('/login', users.logIn);
-router.post('/buy', users.getMovies);
+router.post('/buy', users.UserAddMovie);
+router.get('/view/:id',users.UserWatchMovie);
+//router.post('/buy',user1.getMovies);
 
 module.exports = router;
