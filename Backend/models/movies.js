9c2ee642752ab17709a1a3e4025a1c2825ad5cb9
buka4chocksy <buka4chocksy@gmail.com>
2018-08-17 17:00:56 -0700

    added download in the movies schema

--- Backend/models/movies.js
+++ Backend/models/movies.js
@@ -7,6 +7,7 @@ var movieSchema = mongoose.Schema({
     description: {type: String, required: true},
     releaseYear: Number,
     producer: String,
+    downloads:{type:Number, default:0},
     category: String,
     price: {type: String, required: true},
     image: {type: String},
