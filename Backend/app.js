4657d8f2bb6577a2e9be9cd50c48093714e07343
Ndife <stilesndife@gmail.com>
2018-08-20 09:25:22 +0100

    uploading to heroku

--- Backend/app.js
+++ Backend/app.js
@@ -37,9 +37,9 @@ app.use(bodyParser.json());
 
 Mongoose.Promise = global.Promise;
   
-// Mongoose.connect('mongodb://Admin:ndife123@ds215502.mlab.com:15502/ogenetv' ,{ useNewUrlParser: true });
+Mongoose.connect('mongodb://Admin:ndife123@ds215502.mlab.com:15502/ogenetv' ,{ useNewUrlParser: true });
 
-Mongoose.connect('mongodb://localhost:27017/OgeneTV', { useNewUrlParser: true });
+// Mongoose.connect('mongodb://localhost:27017/OgeneTV', { useNewUrlParser: true });
 
 app.get('/', function(req, res){
     res.json({message:"hello world"});
