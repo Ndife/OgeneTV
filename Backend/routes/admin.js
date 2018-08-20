82d263b37829047659a35662edefa340a655ee9e
Ndife <stilesndife@gmail.com>
2018-08-20 09:14:57 +0100

    added  delete user route

--- Backend/routes/admin.js
+++ Backend/routes/admin.js
@@ -11,6 +11,8 @@ router.get('/blockUser/:id', adminControllers.BlockUser);
 router.get('/unblockUser/:id', adminControllers.unBlockUser);
 router.get('/getAllAdmin',adminControllers.getAllAdmin);
 router.get('/searchAdmin/:value',adminControllers.searchAdmin)
-      .get('searchUser/:value',adminControllers.searchUser);
-
-module.exports = router; 
+      .get('/searchUser/:value',adminControllers.searchUser)
+      .get('/deleteUser/:id',adminControllers.deleteUser)
+      .get('/deleteUser/:id',adminControllers.deleteUser)
+     
+module.exports = router;   
