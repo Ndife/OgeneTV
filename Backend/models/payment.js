8a4a2dd758776d727f2bb3a13931d9005a7d7f7f
Ndife <stilesndife@gmail.com>
2018-08-31 12:20:55 +0100

    modified the payment schema

--- Backend/models/payment.js
+++ Backend/models/payment.js
@@ -2,8 +2,6 @@ var mongoose = require('mongoose');
 var paymentSchema = mongoose.Schema({
     user:{type:String, required:true},
     movie:{type:String, required:true},
-    mail:{type:String, required:true},
-    amount:Number,
     time:Date,
     refNo:{type:String, required:true},
     Status:{type:Boolean, required:true}
