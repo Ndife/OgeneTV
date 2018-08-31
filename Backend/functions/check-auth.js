ba43835f8f2844d5e951368f8a983247fea07bf8
Ndife <32971118+Ndife@users.noreply.github.com>
2018-08-31 12:03:15 +0100

    Update check-auth.js
    modified the authentication methods

--- Backend/functions/check-auth.js
+++ Backend/functions/check-auth.js
@@ -1,9 +1,25 @@
 const jwt = require('jsonwebtoken');
 const secret = require('../functions/secret')
-module.exports = (req,res,next)=>{
+module.exports.user = (req,res,next)=>{
     try {
         const token = req.headers.authorization;
-        const decode = jwt.verify(token, secret.key);
+        const decode = jwt.verify(token, secret.userKey);
+        req.userData = decode;
+        next();
+    } catch (error) {
+        res.status(401).json({
+            message: 'forbidden'
+        });
+    }
+
+    
+}   
+
+
+module.exports.admin = (req,res,next)=>{
+    try {
+        const token = req.headers.authorization;
+        const decode = jwt.verify(token, secret.adminKey);
         req.userData = decode;
         next();
     } catch (error) {
