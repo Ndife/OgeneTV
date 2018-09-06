a4d005fb52a08b5b69b23176c91b13804078db15
Ndife <stilesndife@gmail.com>
2018-09-06 14:59:07 +0100

    added some fileds in review schema

--- Backend/models/review.js
+++ Backend/models/review.js
@@ -3,7 +3,9 @@ var mongoose = require('mongoose');
 
 var reviewSchema = mongoose.Schema({
     time: Date,
-    review: {type: String, required: true}
+    review: {type: String, required: true},
+    email: String,
+    name: String
 })
 
 module.exports = mongoose.model('review', reviewSchema);
